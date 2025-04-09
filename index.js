4e9db0111c49ae4c4bc087262781feb930bb8b37
Gabriel Vitor <mbrdrago@gmail.com>
2025-04-09 17:02:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express")
+const bp = require("body-parser")
+const cors = require("cors")
+const app = express()
+const sqlite3 = require("sqlite3")
+const path = require("path")
+
+app.use(bp.json())
+app.use(bp.urlencoded())
+app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')))
+
+const db = new sqlite3.Database("./db.sqlite")
+
+app.listen(8080, () => {
+    console.log("O servidor está aberto na porta 8080")
+})
+
+db.serialize(()=>(
+    db.run(` CREATE TABLE IF NOT EXISTS Tarefas (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tarefa VARCHAR(50) NOT NULL, 
+    categoria VARCHAR(50)
+) `)
+))
+
+app.get("/tarefas", (req, res) => {
+    db.all(`SELECT * FROM Tarefas`, [],  (err, rows)=>{
+        res.json(rows)
+    })
+})
+
+app.post("/tarefa", (req, res) => {
+    db.run(`INSERT INTO Tarefas (tarefa, categoria) VALUES (?, ?)`, [req.body.tarefa, req.body.categoria])
+})
+
+app.delete("/tarefa/:id", (req, res)=>{
+    db.run(`DELETE FROM Tarefas WHERE id == (?)`, [req.params.id])
+})
+
+app.get("/home", (req, res)=> {
+    res.sendFile('index.html', { root: __dirname })
+})
